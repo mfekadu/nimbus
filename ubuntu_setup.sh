6a62ccd3a554edb7e97f5e577ed9e8cc0b805b7f
Michael Fekadu <mike.fekadu@gmail.com>
2020-06-13 16:55:07 +0000

    adds docker-compose setup

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -38,4 +38,14 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
+echo "now get docker-compose"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.26.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+
 
